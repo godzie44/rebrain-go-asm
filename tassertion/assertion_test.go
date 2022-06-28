b8db676d022f1339903fae2d211c9b47f162a870
godzie44 <godzie@yandex.ru>
2022-06-28 21:21:10 +0300

    add type assertion example

---
+++ tassertion/assertion_test.go
@@ -0,0 +1,68 @@
+package tassertion
+
+import (
+	"math/rand"
+	"runtime"
+	"testing"
+)
+
+type SomeStruct struct {
+	foo int
+	bar int
+}
+
+func initTestData() []any {
+	arr := make([]any, 1000)
+	for i := range arr {
+		arr[i] = SomeStruct{foo: rand.Int(), bar: rand.Int()}
+	}
+	return arr
+}
+
+//go:noinline
+func barNoCheck(c any) int {
+	return c.(SomeStruct).bar
+}
+
+//go:noinline
+func barCheck(c any) int {
+	if c, ok := c.(SomeStruct); ok {
+		return c.bar
+	}
+	return 0
+}
+
+func sumNoCheck(arr []interface{}) int {
+	var sum = 0
+	for _, candidate := range arr {
+		sum += barNoCheck(candidate)
+	}
+	return sum
+}
+
+func sumCheck(arr []interface{}) int {
+	var sum = 0
+	for _, candidate := range arr {
+		sum += barCheck(candidate)
+	}
+	return sum
+}
+
+func BenchmarkSumNoCheck(b *testing.B) {
+	arr := initTestData()
+	for i := 0; i < b.N; i++ {
+		sum := sumNoCheck(arr)
+		runtime.KeepAlive(sum)
+	}
+}
+
+func BenchmarkSumCheck(b *testing.B) {
+	arr := initTestData()
+	for i := 0; i < b.N; i++ {
+		sum := sumCheck(arr)
+		runtime.KeepAlive(sum)
+	}
+}
+
+// go test -bench=. -benchmem -cpuprofile profile.out
+//  go tool pprof rebrain_asm.test profile.out
